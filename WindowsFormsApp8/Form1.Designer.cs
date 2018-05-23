bfcc32113e2f62b70a797e79bf255f599db4a200
DesarrolloWeb <DesarrolloWeb@T06W03>
2018-05-23 17:06:55 +0200

    completo

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(215, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(302, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(215, 133);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(310, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//ejercicio 10.12
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        //pongo aquí la cadena de conexión porque combo y tabla las usarán
+        string cadena = ConfigurationManager.ConnectionStrings["cadenaNorthwind"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargarCombo(); //`lo primero que hago y genero método
+        }
+
+        private void cargarCombo()
+        {
+            string consulta = "select SupplierID, CompanyName from Suppliers"; //escribo la consulta
+            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cadena);
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+
+            //CREAMOS COMBO
+            //añadp fila vacía
+            DataRow fila = tabla.NewRow();
+            fila["CompanyName"].Equals(String.Empty); //lo relleno vacío
+            fila["SupplierID"].Equals (0);
+            tabla.Rows.Add(fila);
+
+            //ordeno la tabla
+            tabla.DefaultView.Sort = tabla.Columns[1].ColumnName;
+
+            //PRopiedad por delante y por detrás porque es combo
+            comboBox1.DisplayMember = tabla.Columns[1].ColumnName; //por delante
+            comboBox1.ValueMember = tabla.Columns[0].ColumnName;//por detrás
+            comboBox1.DataSource = tabla; //suministros los datos
+
+
+
+        }
+        //cuando selecciono de la combo una opcion
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(comboBox1.SelectedIndex)>0) //esto es para que cargue, sino da fallo
+            {
+            int valor = (int)comboBox1.SelectedValue; //capturo lo que está seleccionado y lo casteo
+            CargarGrid(valor); //doy a bombilla para cargar metodo
+            }
+           
+        }
+
+        private void CargarGrid(int valor)
+        {
+            string consulta = "select ProductName , QuantityPerUnit , UnitPrice, UnitsInStock from Products WHERE SupplierID = '" + valor + "'";
+            SqlDataAdapter adapter = new SqlDataAdapter(consulta, cadena);
+            DataTable tabla = new DataTable();
+            adapter.Fill(tabla);
+            dataGridView1.DataSource = tabla;
+            dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);// ajusta el tamaño de la celda
+            
+        }
+    }
+}
